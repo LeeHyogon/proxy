f2f9e8e457693d35a32cc35a85542af08fffe698
이효곤 <gon109m@gmail.com>
2023-05-30 22:49:05 +0900

    인터페이스 기반 프록시-적용

--- src/main/java/hello/proxy/app/v2/OrderServiceV2.java
+++ src/main/java/hello/proxy/app/v2/OrderServiceV2.java
@@ -1,9 +1,9 @@
 package hello.proxy.app.v2;
 
-public class OrderServiceV3 {
+public class OrderServiceV2 {
     private final OrderRepositoryV2 orderRepository;
 
-    public OrderServiceV3(OrderRepositoryV2 orderRepository) {
+    public OrderServiceV2(OrderRepositoryV2 orderRepository) {
         this.orderRepository = orderRepository;
     }
 
--- src/main/java/hello/proxy/app/v3/OrderControllerV3.java
+++ src/main/java/hello/proxy/app/v3/OrderControllerV3.java
@@ -1,7 +1,6 @@
 package hello.proxy.app.v3;
 
 
-import hello.proxy.app.v2.OrderServiceV3;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
